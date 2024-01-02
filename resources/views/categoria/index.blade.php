7de2d6d406d243a6c0a960b02c7f82db87737036
Heryck <heryckmota@gmail.com>
2024-01-02 16:12:44 -0300

    Adicionando aviso de dados não encontrados

--- resources/views/categoria/index.blade.php
+++ resources/views/categoria/index.blade.php
@@ -9,59 +9,60 @@
 
 
 @section('content')
-<div class="d-flex">
+    <div class="d-flex">
 
-    <div class="input-group mb-3">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></span>
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></span>
+            </div>
+            <form class="mr-2" id="formSearch" action="{{ route('categoria.index') }}" method="GET">
+                <input type="text" id="search" name="search" class="form-control" placeholder="" aria-label=""
+                    aria-describedby="basic-addon1">
+            </form>
+            <a href="{{ route('produto.index') }}" class="btn btn-primary">Limpar busca</a>
+
         </div>
-        <form class="mr-2" id="formSearch" action="{{route('produto.index')}}" method="GET">
-            <input type="text" id="search" name="search" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
-        </form>
-        <a href="{{ route('produto.index') }}" class="btn btn-primary">Limpar busca</a>
-
-      </div>
-    {{$categorias->links() }}
-</div>
-    <table id="categoriaTable" class="table table-striped table-hover">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Nome</th>
-                <th>Descrição</th>
-                <th class="d-flex justify-content-center">Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($categorias as $categoria)
+        {{ $categorias->links() }}
+    </div>
+    @if (!$categorias->isEmpty())
+        <table id="categoriaTable" class="table table-striped table-hover">
+            <thead>
                 <tr>
-                    <td>{{ $categoria->id }}</td>
-                    <td>{{ $categoria->nome }}</td>
-                    <td>{{ $categoria->descricao }}</td>
-                    <td class="d-flex  justify-content-around">
-                        <button data-url="{{ $categoria->url_capa }}" data-nome="{{ $categoria->nome }}" type="button"
-                            class="btn btn-primary infoFoto mr-1">
-                            <i class="fas fa-info"></i>
-                        </button>
-                        <a href="{{route('categoria.form',$categoria->id)}}" type="button" class="btn btn-warning mr-1 editModal"><i
-                                class="fas fa-edit"></i></a>
-                        <form method="POST" action="{{ route('categoria.delete', $categoria->id) }}"
-                            enctype="multipart/form-data">
-                            @method('DELETE')
-                            @csrf
-                            <button type="submit" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
-
-                        </form>
-
-                    </td>
+                    <th>Id</th>
+                    <th>Nome</th>
+                    <th>Descrição</th>
+                    <th class="d-flex justify-content-center">Ações</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <!-- Button trigger modal -->
+            </thead>
+            <tbody>
+                @foreach ($categorias as $categoria)
+                    <tr>
+                        <td>{{ $categoria->id }}</td>
+                        <td>{{ $categoria->nome }}</td>
+                        <td>{{ $categoria->descricao }}</td>
+                        <td class="d-flex  justify-content-around">
+                            <button data-url="{{ $categoria->url_capa }}" data-nome="{{ $categoria->nome }}" type="button"
+                                class="btn btn-primary infoFoto mr-1">
+                                <i class="fas fa-info"></i>
+                            </button>
+                            <a href="{{ route('categoria.form', $categoria->id) }}" type="button"
+                                class="btn btn-warning mr-1 editModal"><i class="fas fa-edit"></i></a>
+                            <form method="POST" action="{{ route('categoria.delete', $categoria->id) }}"
+                                enctype="multipart/form-data">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
 
+                            </form>
 
-    <!-- Modal -->
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <x-not-found />
+    @endif
     <div class="modal fade" id="fotoModal" tabindex="-1" aria-labelledby="fotoModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
@@ -84,7 +85,7 @@
             </div>
         </div>
     </div>
- 
+
 
 
 
@@ -96,6 +97,9 @@
             $('#fotoCategoria').attr('src', this.dataset.url)
             $('#nomeFotoCategoria').text(this.dataset.nome)
             $('#fotoModal').modal('show')
+        })
+        document.getElementById('search').addEventListener('change', function() {
+            document.getElementById('formSearch').submit()
         })
     </script>
 @endpush
